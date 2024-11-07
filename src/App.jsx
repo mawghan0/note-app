6b8cdb790a6296c1168a3d53db79ede800f5f779
Muhammad Yusuf Habibie <myh481813@gmail.com>
2024-11-07 13:49:06 +0700

    add: formInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { getInitialData, showFormattedDate } from "./utils";
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import FormInput from "./components/FormInput";
 import ListItem from "./components/ListItem";
 
 function App() {
   const [datas, setDatas] = useState(getInitialData);
   const [filteredDatas, setFilteredDatas] = useState(datas);
-
   return (
     <>
       <SearchBar datas={datas} setFilteredDatas={setFilteredDatas} />
       <br />
-      <ListItem filteredDatas={filteredDatas} showFormattedDate={showFormattedDate}/>
+      <FormInput
+        datas={datas}
+        setDatas={setDatas}
+        setFilteredDatas={setFilteredDatas}
+      />
+      <br />
+      <ListItem
+        filteredDatas={filteredDatas}
+        showFormattedDate={showFormattedDate}
+      />
     </>
   );
 }
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,57 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+function FormInput({ datas, setDatas, setFilteredDatas }) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addData = (event) => {
+    event.preventDefault();
+    const newNote = {
+      id: +new Date(),
+      title: title,
+      body: body,
+      createdAt: new Date().toISOString(),
+      archived: false,
+    };
+    const updatedData = [...datas, newNote];
+    setDatas(updatedData);
+    setFilteredDatas(updatedData);
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <>
+      <form onSubmit={addData}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Body"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        />
+        <br />
+        <button type="submit">Add Note</button>
+      </form>
+    </>
+  );
+}
+
+FormInput.propTypes = {
+  datas: PropTypes.array.isRequired,
+  setDatas: PropTypes.func.isRequired,
+  setFilteredDatas: PropTypes.func.isRequired,
+};
+
+export default FormInput;
